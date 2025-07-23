5f4a3f43d3b2cf00676e6b3a69634aef37363a73
debyendu <debyendu03@gmail.com>
2025-07-23 11:45:55 +0530

    Improve responsiveness and styling of client logos in ourClients component

--- js/components/ourClients.js
+++ js/components/ourClients.js
@@ -13,21 +13,34 @@ export function ourClients() {
                 <div class="w-100">
                     <div
                         style="width: fit-content"
-                        class="mx-auto my-3 px-5 py-4 border border-primary rounded-4 shadow rounded"
+                        class="mx-auto my-3 px-2 px-sm-3 px-md-5 py-4 border border-primary rounded-4 shadow rounded"
                     >
-                        <div class="d-flex justify-content-evenly align-items-center gap-3">
-                            <img src="asset/logos/cpLOG4.webp" alt="Infosys" height="42" class="p-1" />
-                            <img src="asset/logos/cpLOG6.webp" alt="Bosch" height="37" class="p-1" />
-                            <img src="asset/logos/cpLOG3.webp" alt="Capgemini" height="40" class="p-1" />
-                            <img src="asset/logos/cpLOG1.webp" alt="KPMG" height="45" class="p-1" />
-                            <img src="asset/logos/cpLOG2.webp" alt="Accenture" height="40" class="p-1" />
+                        <div class="d-flex flex-wrap justify-content-center justify-content-md-evenly align-items-center gap-2 gap-md-3 mb-3">
+                            <img src="asset/logos/cpLOG4.webp" alt="Infosys" height="42" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG6.webp" alt="Bosch" height="37" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG3.webp" alt="Capgemini" height="40" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG1.webp" alt="KPMG" height="45" class="p-1 client-logo d-none d-sm-block" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG2.webp" alt="Accenture" height="40" class="p-1 client-logo d-none d-md-block" style="max-width: 80px;" />
                         </div>
-                        <div class="d-flex justify-content-evenly align-items-center gap-3">
-                            <img src="asset/logos/cpLOG5.webp" alt="Cognizant" height="32" class="p-1" />
-                            <img src="asset/logos/cpLOG8.webp" alt="Deloitte" height="32" class="p-1" />
-                            <img src="asset/logos/cpLOG9.webp" alt="Hexaware" height="60" class="p-1" />
-                            <img src="asset/logos/cpLOG10.webp" alt="Thomson Reuters" height="75" class="p-1" />
-                            <img src="asset/logos/cpLOG12.webp" alt="Wells Fargo" height="65" class="p-1" />
+                        <div class="d-flex flex-wrap justify-content-center justify-content-md-evenly align-items-center gap-2 gap-md-3">
+                            <img src="asset/logos/cpLOG1.webp" alt="KPMG" height="45" class="p-1 client-logo d-block d-sm-none" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG2.webp" alt="Accenture" height="40" class="p-1 client-logo d-block d-md-none" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG5.webp" alt="Cognizant" height="32" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG8.webp" alt="Deloitte" height="32" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG9.webp" alt="Hexaware" height="60" class="p-1 client-logo d-none d-sm-block" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG10.webp" alt="Thomson Reuters" height="75" class="p-1 client-logo d-none d-md-block" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG12.webp" alt="Wells Fargo" height="65" class="p-1 client-logo d-none d-lg-block" style="max-width: 80px;" />
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center gap-2 d-block d-sm-none mt-3">
+                            <img src="asset/logos/cpLOG9.webp" alt="Hexaware" height="60" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG10.webp" alt="Thomson Reuters" height="75" class="p-1 client-logo" style="max-width: 80px;" />
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center gap-2 d-none d-sm-block d-md-none mt-3">
+                            <img src="asset/logos/cpLOG10.webp" alt="Thomson Reuters" height="75" class="p-1 client-logo" style="max-width: 80px;" />
+                            <img src="asset/logos/cpLOG12.webp" alt="Wells Fargo" height="65" class="p-1 client-logo" style="max-width: 80px;" />
+                        </div>
+                        <div class="text-center d-none d-md-block d-lg-none mt-3">
+                            <img src="asset/logos/cpLOG12.webp" alt="Wells Fargo" height="65" class="p-1 client-logo" style="max-width: 80px;" />
                         </div>
                     </div>
                 </div>
